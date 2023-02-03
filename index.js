665fa4d692279031e32cb35ef3e5d80dc6d3ab8c
pooja kumawat <pkumawat009876@gmail.com>
2023-02-03 22:23:26 +0530

    basic function and variables created

---
+++ index.js
@@ -0,0 +1,25 @@
+let hr = 0;
+let min = 0;
+let sec = 0;
+//timer is false means timer is stop currently
+let timer = false;
+
+// Function for start watch
+function startButton() {
+
+}
+
+// Function for stop watch
+function stopButton() {
+
+}
+
+// Function for restart watch
+function restartButton() {
+
+}
+
+//main function
+function stopwatch(){
+
+}
