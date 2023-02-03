801cc321496e635479980174d4122d659c2c8fb9
pooja kumawat <pkumawat009876@gmail.com>
2023-02-03 22:50:06 +0530

    created basic logic

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 let hr = 0;
 let min = 0;
 let sec = 0;
+
 //timer is false means timer is stop currently
 let timer = false;
 
 // Function for start watch
 function startButton() {
-
+    if (timer == false) {
+        timer = true;
+        stopwatch();
+    };
 }
 
 // Function for stop watch
 function stopButton() {
-
+    timer = false;
 }
 
 // Function for restart watch
@@ -19,7 +23,12 @@ function restartButton() {
 
 }
 
-//main function
-function stopwatch(){
+//main function that work
+function stopwatch() {
+    if (timer == true) {
 
+
+        
+        setTimeout('stopwatch()', 1000);
+    }
 }
